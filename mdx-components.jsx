8df9d3d7ffa4fbf2fb4210cab7e184b2f5744223
Alyssa Holland <alysam0071@gmail.com>
2025-01-07 21:56:06 -0500

    rename file to have .jsx extension

--- mdx-components.jsx
+++ mdx-components.jsx
@@ -1,8 +1,7 @@
-import type { MDXComponents } from 'mdx/types'
 
 //TODO: Reference docs to customize styles for all MDX files
 // - https://nextjs.org/docs/pages/building-your-application/configuring/mdx#using-custom-styles-and-components
-export function useMDXComponents(components: MDXComponents): MDXComponents {
+export function useMDXComponents(components) {
   return {
     ...components,
   }
